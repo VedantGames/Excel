e5bb5d11ed6020ecbd198b0ded297ddb652646bd
Vedant Games <guptavedant2549@gmail.com>
2024-08-09 18:05:17 +0530

    Final Updates

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TitleBar() {
   return (
-    <div className='bg-black text-white flex items-center justify-center h-10'>
+    <div className='bg-black text-white text-sm flex items-center justify-center pt-2'>
       Vedant.xlsx - Excel
     </div>
   )
